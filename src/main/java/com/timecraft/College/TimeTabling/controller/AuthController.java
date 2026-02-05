89d042ff2df1c03c4deca34893a2f0aa9658d194
LexAsf22 <vanphillip.tamayo@lorma.edu>
2026-02-05 18:08:19 +0800

    meow

--- src/main/java/com/timecraft/College/TimeTabling/controller/AuthController.java
+++ src/main/java/com/timecraft/College/TimeTabling/controller/AuthController.java
@@ -1,7 +1,12 @@
 package com.timecraft.College.TimeTabling.controller;
 
 import com.timecraft.auth.api.AuthApi;
+import com.timecraft.auth.dto.LoginRequest;
+import com.timecraft.auth.dto.AuthResponse;
+import org.springframework.web.bind.annotation.RestController;
 
-public class AuthController implements AuthApi{
-    
+@RestController
+public class AuthController implements AuthApi {
+
+
 }
